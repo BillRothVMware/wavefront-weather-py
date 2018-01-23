a98cb0bf94a1ee24d35c0539c7d74fe6d5649ea1
Bill Roth <broth@vmware.com>
2018-01-23 14:59:10 -0800

    added comments and attribution

--- wu-once.py
+++ wu-once.py
@@ -1,3 +1,11 @@
+#
+# Pulling data from WUnderground and sending to Wavefront by Python
+#
+# Core original code written by Durren Shen @durrenshen 
+# in a blog at https://www.wavefront.com/weather-metrics/
+# 
+# Cleaned up by Bill Roth @BillRothVMware
+#
 import urllib2
 import json
 import logging
