451c9502b36c99f7c3f2341e2683a94700a8ea47
Bill Roth <broth@vmware.com>
2018-08-06 13:31:01 -0700

    updated file with documentation and fix errors
    updated file with documentation and fix errors. Added some docs

--- intemp.sh
+++ intemp.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
 
-t=`curl -s 'https://api.ambientweather.net/v1/devices?applicationKey=<app-key>&apiKey=<api-key)' | jq '.[0].lastData.tempinf'`
-echo "broth.home.weather.intempf" ${t} "source=ambient dc=willow-glen user=broth
-@vmware.com"
+#
+# first, call the api, then parse for the inside temperature
+t=`curl -s 'https://api.ambientweather.net/v1/devices?applicationKey=<app-key>&apiKey=<api-key>' | jq '.[0].lastData.tempinf'`
+#
+# Output in Wavefront format: http://wavefront.com/sign-up/
+#
+echo "broth.home.weather.intempf" ${t} "source=ambient dc=willow-glen user=broth@vmware.com"
